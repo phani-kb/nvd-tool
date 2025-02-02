15e6770f73f5c488810d63a42d2cd269d1b935a0
Phani K <192951055+phani-kb@users.noreply.github.com>
2025-02-01 23:15:54 -0500

    add cpe, cwe uri download commands

---
+++ src/main/java/com/github/phanikb/nvd/cli/uri/download/CweUriDownloadCommand.java
@@ -0,0 +1,29 @@
+package com.github.phanikb.nvd.cli.uri.download;
+
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+
+import com.github.phanikb.nvd.common.NvdProperties;
+import com.github.phanikb.nvd.enums.FeedType;
+
+@CommandLine.Command(
+        name = "cwe",
+        mixinStandardHelpOptions = true,
+        subcommands = {CommandLine.HelpCommand.class},
+        description = "Download CWE data.")
+public class CweUriDownloadCommand extends BaseUriDownloadCommand implements Callable<Integer> {
+
+    @Override
+    public String[] getUrls() {
+        NvdProperties nvdProperties = NvdProperties.getInstance();
+        String[] urls = new String[1];
+        urls[0] = nvdProperties.getNvd().getCwe().getUrl();
+        return urls;
+    }
+
+    @Override
+    public FeedType getFeedType() {
+        return FeedType.CWE;
+    }
+}
