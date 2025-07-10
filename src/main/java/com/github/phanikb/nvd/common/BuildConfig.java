5cf605895983cb4c457b76130fce68cb13f2f94c
Phani K <192951055+phani-kb@users.noreply.github.com>
2025-07-10 17:49:07 -0400

    update security.yml and pom.xml for versioning and remove old workflow cleanup

--- src/main/java/com/github/phanikb/nvd/common/BuildConfig.java
+++ src/main/java/com/github/phanikb/nvd/common/BuildConfig.java
@@ -2,5 +2,5 @@ package com.github.phanikb.nvd.common;
 
 public class BuildConfig {
     public static final String ARTIFACT_ID = "nvd-tool";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.0-SNAPSHOT";
 }
